70b85452ff347b2dc7280c095c85915ea906bb6f
erhankur <erhankurubas@gmail.com>
2022-10-29 22:29:21 +0200

    fix subprocess and lib paths

--- tools/custom_ota_gen.py
+++ tools/custom_ota_gen.py
@@ -124,7 +124,7 @@ def get_app_name():
     return ''
 
 def update_ddelta_library_path():
-    os.environ['LD_LIBRARY_PATH'] = "bootloader_components/tools/lib"
+    os.environ['LD_LIBRARY_PATH'] = "bootloader_components/tools/libdivsufsort"
 
 def main():
     parser = argparse.ArgumentParser()
@@ -187,7 +187,7 @@ def main():
         if delta_type == 'ddelta':
             update_ddelta_library_path()
             uncompressed_patch = ''.join([cpmoressed_app_directory,'/','patch'])
-            ret = subprocess.call('bootloader_components/tools/ddelta_generate {0} {1} {2}'.format(base_file, src_file, uncompressed_patch), shell = True)
+            ret = subprocess.call('bootloader_components/tools/esp_gen_patch/ddelta_generate {0} {1} {2}'.format(base_file, src_file, uncompressed_patch), shell = True)
             # print('xz compress cmd return: {}'.format(ret))
             if ret:
                 raise Exception("ddelta_gen cmd failed")
